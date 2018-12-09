a98cf0e7e39f6919edfcdb5b78d161090988e102
vp <coervivek@gmail.com>
2018-12-09 23:53:41 +0530

    removed extra space

--- DS-Algo/src/reverse/string/ReverseString.java
+++ DS-Algo/src/reverse/string/ReverseString.java
@@ -8,7 +8,6 @@ public class ReverseString {
 		int arr[] = { 7, 6, 1, 2, 3, 4, 5, 6 };
 		System.out.println(Arrays.toString(arr));
 		rvereseArray(arr, 0, arr.length - 1); // end is n-1
-		
 		System.out.print("Reversed array is \n");
 		System.out.println(Arrays.toString(arr));
 	}
